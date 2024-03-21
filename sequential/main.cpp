91b908a4bec8551221236712550a6be96a22538c
thoeschler <thilo.hoeschler@gmx.de>
2024-03-21 20:14:59 +0100

    add error computation

--- sequential/main.cpp
+++ sequential/main.cpp
@@ -2,6 +2,7 @@
 #include "crs.hpp"
 #include "la.hpp"
 #include "finite_diff.hpp"
+#include "utils.hpp"
 
 #include <iostream>
 #include <vector>
@@ -13,7 +14,7 @@ double bc(double x, double y) {
 
 int main(int argc, char** argv) {
     // number of nodes
-    const std::size_t Nx = 1200, Ny = 1200;
+    const std::size_t Nx = 8, Ny = 16;
     const std::size_t Nxt = Nx - 2, Nyt = Ny - 2;
     UnitSquareGrid grid(Nx, Ny);
 
@@ -26,10 +27,18 @@ int main(int argc, char** argv) {
     // assemble matrix
     CRSMatrix A;
     assemble_matrix(A, grid);
+    A.print_values();
 
     // solve system
     std::vector<double> u(Nxt * Nyt);
     cg(A, b, u);
+    std::cout << std::endl;
 
+    // compute error
+    double error = compute_l1_error(u, bc, grid);
+    std::cout << "l1 error: " << error << "\n";
+    error = compute_linf_error(u, bc, grid);
+    std::cout << "linf error: " << error << "\n";
+    
     return 0;
 }
---
+++ sequential/utils.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "grid.hpp"
+
+#include <functional>
+#include <vector>
+#include <assert.h>
+
+double compute_l1_error(std::vector<double> const& u, std::function<double(double, double)> u_ana, UnitSquareGrid const& grid) {
+    assert(u.size() == (grid.Nx - 2) * (grid.Ny - 2));
+    std::size_t Nxt = grid.Nx - 2, Nyt = grid.Ny - 2;
+    double hx = 1.0 / (grid.Nx - 1), hy = 1.0 / (grid.Ny - 1);
+    double error = 0.0;
+    double x, y;
+    for (std::size_t idx = 0; idx < Nxt; ++idx) {
+        for (std::size_t idy = 0; idy < Nyt; ++idy) {
+            x = (idx + 1) * hx;
+            y = (idy + 1) * hy;
+            error += std::abs(u[idy * Nxt + idx] - u_ana(x, y)); 
+        }
+    }
+    return error;
+}
+
+double compute_linf_error(std::vector<double> const& u, std::function<double(double, double)> u_ana, UnitSquareGrid const& grid) {
+    assert(u.size() == (grid.Nx - 2) * (grid.Ny - 2));
+    std::size_t Nxt = grid.Nx - 2, Nyt = grid.Ny - 2;
+    double hx = 1.0 / (grid.Nx - 1), hy = 1.0 / (grid.Ny - 1);
+    double error = 0.0;
+    double x, y, e;
+    for (std::size_t idx = 0; idx < Nxt; ++idx) {
+        for (std::size_t idy = 0; idy < Nyt; ++idy) {
+            x = (idx + 1) * hx;
+            y = (idy + 1) * hy;
+            e = std::abs(u[idy * Nxt + idx] - u_ana(x, y));
+            if (e > error) error = e;
+        }
+    }
+    return error;
+}
