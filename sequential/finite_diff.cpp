ae9bf739d7239d5c17acd2e6bf2eabdfae441c9d
thoeschler <thilo.hoeschler@gmx.de>
2024-03-21 20:16:15 +0100

    fix bug in matrix assembly

--- sequential/finite_diff.cpp
+++ sequential/finite_diff.cpp
@@ -56,16 +56,16 @@ void assemble_matrix(CRSMatrix &A, UnitSquareGrid const& grid) {
     const double hx2 = hx * hx;
     const double hy2 = hy * hy;
 
-    const double diagonal_value = - 2.0 / hx2 - 2.0 / hy2;
+    const double diagonal_value = 2.0 / hx2 + 2.0 / hy2;
     std::size_t node_number;
-    for (std::size_t idx = 0; idx < Nxt; ++idx) {
-        for (std::size_t idy = 0; idy < Nyt; ++idy) {
-            node_number = idx + Nxt * idy;
-            if (idy > 0) { A.append(1.0 / hy2, node_number - Nxt); }
-            if (idx > 0) { A.append(1.0 / hx2, node_number - 1); }
+    for (std::size_t idy = 0; idy < Nyt; ++idy) {
+        for (std::size_t idx = 0; idx < Nxt; ++idx) {
+            node_number = idy * Nxt + idx;
+            if (idy > 0) { A.append(- 1.0 / hy2, node_number - Nxt); }
+            if (idx > 0) { A.append(- 1.0 / hx2, node_number - 1); }
             A.append(diagonal_value, node_number);
-            if (idx < Nxt - 1) { A.append(1.0 / hx2, node_number + 1); }
-            if (idy < Nyt - 1) { A.append(1.0 / hy2, node_number + Nxt); }
+            if (idx < Nxt - 1) { A.append(- 1.0 / hx2, node_number + 1); }
+            if (idy < Nyt - 1) { A.append(- 1.0 / hy2, node_number + Nxt); }
 
             A.next_row();
         }
