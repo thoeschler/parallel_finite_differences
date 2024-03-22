6f3cfb6db108e6221de5f3120f0763edbc74b298
thoeschler <thilo.hoeschler@gmx.de>
2024-03-22 12:11:37 +0100

    remove redundant code

--- sequential/finite_diff.hpp
+++ sequential/finite_diff.hpp
@@ -6,15 +6,6 @@
 #include <vector>
 #include <functional>
 
-// class PoissonFiniteDifferenceProblem2D {
-//     private:
-//         std::function<double(double, double)> boundary_condition;
-//         FiniteDifferenceCRSMatrix _A;
-//         std::vector<double> u;
-//     public:
-
-// };
-
 void assemble_rhs(std::vector<double> &b, UnitSquareGrid const& grid, std::function<double(double, double)> bc);
 
 void assemble_matrix(CRSMatrix &A, UnitSquareGrid const& grid);
