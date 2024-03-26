9c88844638d7be3651f03805c477954a8b47e646
thoeschler <thilo.hoeschler@gmx.de>
2024-03-26 16:43:59 +0100

    switch order in matrix assembly

--- finite_diff.cpp
+++ finite_diff.cpp
@@ -72,22 +72,25 @@ void assemble_local_matrix(CRSMatrix &A_loc, std::vector<int> const& dims, std::
     for (std::size_t idy_loc = 0; idy_loc < local_grid.Ny; ++idy_loc) {
         for (std::size_t idx_loc = 0; idx_loc < local_grid.Nx; ++idx_loc) {
             col_self = (idy_loc + pad_down) * Nxt + idx_loc + pad_left;
+
             if (py > 0 || idy_loc > 0) { // lower neighbor
                 col_down = col_self - Nxt;
                 A_loc.append(- 1.0 / hy2, col_down);
                 }
+            if (py < dims[0] - 1 || idy_loc < local_grid.Ny - 1) { // upper neighbor
+                col_up = col_self + Nxt;
+                A_loc.append(- 1.0 / hy2, col_up);
+                }
+
+            A_loc.append(diagonal_value, col_self);
+
             if (px > 0 || idx_loc > 0) { // left neighbor
                 col_left = col_self - 1;
                 A_loc.append(- 1.0 / hx2, col_left);
                 }
-            A_loc.append(diagonal_value, col_self);
             if (px < dims[1] - 1 || idx_loc < local_grid.Nx - 1) { // right neighbor
                 col_right = col_self + 1;
                 A_loc.append(- 1.0 / hx2, col_right);
-                }
-            if (py < dims[0] - 1 || idy_loc < local_grid.Ny - 1) { // upper neighbor
-                col_up = col_self + Nxt;
-                A_loc.append(- 1.0 / hy2, col_up);
                 }
 
             A_loc.next_row();
