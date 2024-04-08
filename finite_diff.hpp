d64348b304a729f4f398229f19ea70b68f3b50d4
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-04-08 16:49:50 +0200

    change indentation

--- finite_diff.hpp
+++ finite_diff.hpp
@@ -8,8 +8,8 @@
 #include <mpi.h>
 
 void assemble_local_rhs(std::vector<double> &b_loc, UnitSquareGrid const& global_grid, 
-                        LocalUnitSquareGrid const& local_grid, std::vector<int> const &coords,
-                        std::vector<int> const& dims, std::function<double(double, double)> boundary_condition);
+    LocalUnitSquareGrid const& local_grid, std::vector<int> const &coords, std::vector<int> const& dims,
+    std::function<double(double, double)> boundary_condition);
 
 void assemble_local_matrix(CRSMatrix &A_loc, std::vector<int> const& dims, std::vector<int> const& coords,
     UnitSquareGrid const& global_grid, LocalUnitSquareGrid const& local_grid);
