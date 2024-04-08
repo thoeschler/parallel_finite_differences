cc9f32e2791acb8781b52a8a7b56540203bcd0a2
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-04-08 16:46:10 +0200

    rename local matrix

--- finite_diff.hpp
+++ finite_diff.hpp
@@ -11,5 +11,5 @@ void assemble_local_rhs(std::vector<double> &b_loc, UnitSquareGrid const& global
                         LocalUnitSquareGrid const& local_grid, std::vector<int> const &coords,
                         std::vector<int> const& dims, std::function<double(double, double)> boundary_condition);
 
-void assemble_local_matrix(CRSMatrix &A, std::vector<int> const& dims, std::vector<int> const& coords,
+void assemble_local_matrix(CRSMatrix &A_loc, std::vector<int> const& dims, std::vector<int> const& coords,
     UnitSquareGrid const& global_grid, LocalUnitSquareGrid const& local_grid);
