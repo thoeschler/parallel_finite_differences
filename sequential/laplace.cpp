58e1026d15eaa22526f7bcdd750cfb30428ce15c
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-04-18 10:48:54 +0200

    rename main, grid size as input

--- sequential/laplace.cpp
+++ sequential/laplace.cpp
@@ -7,14 +7,13 @@
 #include <iostream>
 #include <vector>
 
-#define Nx 1000
-#define Ny 1000
-
 double bc(double x, double y) {
     return x + y;
 }
 
-int main() {
+int main(int argc, char **argv) {
+    std::size_t Nx = atoi(argv[1]);
+    std::size_t Ny = atoi(argv[2]);
     // number of nodes
     const std::size_t Nxt = Nx - 2, Nyt = Ny - 2;
     UnitSquareGrid grid(Nx, Ny);
@@ -34,7 +33,7 @@ int main() {
 
     // compute error
     double error = compute_l1_error(u, bc, grid);
-    std::cout << "l1 error: " << error << "\n";
+    std::cout << "\nl1 error: " << error << "\n";
     error = compute_linf_error(u, bc, grid);
     std::cout << "linf error: " << error << "\n";
     
