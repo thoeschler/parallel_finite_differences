97940545ae1c9107dd86faaa45519e150378aa6e
Thilo Höschler <thilo.hoeschler@gmx.de>
2024-05-20 21:16:09 +0200

    stylistic changes, overwrite output vector within matvec functions

--- include/finite_diff.hpp
+++ include/finite_diff.hpp
@@ -7,13 +7,13 @@
 #include <functional>
 #include <mpi.h>
 
-void assemble_rhs(std::vector<double> &b, UnitSquareGrid const& grid, std::function<double(double, double)> bc);
+void assemble_rhs(std::vector<double> &b, const UnitSquareGrid &grid, std::function<double(double, double)> bc);
 
-void assemble_matrix(CRSMatrix &A, UnitSquareGrid const& grid);
+void assemble_matrix(CRSMatrix &A, const UnitSquareGrid &grid);
 
 void assemble_local_rhs(std::vector<double> &b_loc, UnitSquareGrid const& global_grid, 
-    LocalUnitSquareGrid const& local_grid, std::vector<int> const &coords, std::vector<int> const& dims,
+    const LocalUnitSquareGrid &local_grid, const std::vector<int> &coords, const std::vector<int> &dims,
     std::function<double(double, double)> boundary_condition);
 
-void assemble_local_matrix(CRSMatrix &A_loc, std::vector<int> const& dims, std::vector<int> const& coords,
-    UnitSquareGrid const& global_grid, LocalUnitSquareGrid const& local_grid);
+void assemble_local_matrix(CRSMatrix &A_loc, const std::vector<int> &dims, const std::vector<int> &coords,
+    const UnitSquareGrid &global_grid, const LocalUnitSquareGrid &local_grid);
--- include/grid.hpp
+++ include/grid.hpp
@@ -4,8 +4,7 @@
 #include <iostream>
 
 struct UnitSquareGrid {
-    std::size_t Nx; // number of grid points in x-direction
-    std::size_t Ny; // number of grid points in y-direction
+    std::size_t Nx, Ny; // number of grid points in x- and y-directions
     UnitSquareGrid(std::size_t Nx_, std::size_t Ny_) : Nx{Nx_}, Ny{Ny_} {};
     void info() const;
 };
@@ -13,15 +12,13 @@ struct UnitSquareGrid {
 class LocalUnitSquareGrid {
     private:
         std::tuple<std::size_t, std::size_t, std::size_t, std::size_t> get_local_dimensions(
-            UnitSquareGrid const& global_grid, std::vector<int> const& dims, std::vector<int> const& coords
+            const UnitSquareGrid &global_grid, const std::vector<int> &dims, const std::vector<int> &coords
         );
     public:
-        std::size_t Nx; // number of local grid points in x-direction
-        std::size_t Ny; // number of local grid points in y-direction
-        std::size_t idx_glob_start; // starting x-index in global grid
-        std::size_t idy_glob_start; // starting y-index in global grid
+        std::size_t Nx, Ny; // number of local grid points in x- and y-directions
+        std::size_t idx_glob_start, idy_glob_start; // starting x- and y-indices in global grid
         bool has_bottom_neighbor, has_top_neighbor, has_left_neighbor, has_right_neighbor;
-        LocalUnitSquareGrid(UnitSquareGrid const& global_grid, std::vector<int> const& dims, std::vector<int> const& coords) {
+        LocalUnitSquareGrid(const UnitSquareGrid &global_grid, const std::vector<int> &dims, const std::vector<int> &coords) {
             std::tie(Nx, Ny, idx_glob_start, idy_glob_start) = get_local_dimensions(global_grid, dims, coords);
             has_top_neighbor = coords[0] > 0;
             has_bottom_neighbor = coords[0] < dims[0] - 1;
